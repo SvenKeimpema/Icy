3a5287cbd80c22808e88c7260f766a232182a11d
Sven <61794662+SvenKeimpema@users.noreply.github.com>
2023-02-10 17:48:37 +0100

    Delete icy directory

--- icy/code.ice
+++
@@ -1,5 +0,0 @@
-if(7+7)
-    print(1)
-else
-    print(2)
-end
--- icy/ice.py
+++
@@ -1,6 +0,0 @@
-from iceParser import Parser
-import compiler
-
-if __name__ == "__main__":
-    parser = Parser()
-    program = compiler.compileProgram(parser.produceAST())
